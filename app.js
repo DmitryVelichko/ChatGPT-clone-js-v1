06ce0e6d5415e79c91516f64878fcfd472a01406
Dmitry Velichko <soleil-2005@mail.ru>
2023-05-13 13:32:37 +0800

    New Commit

--- app.js
+++ app.js
@@ -9,3 +9,18 @@ async function getMessage() {
             'Authorization': `Bearer ${API_KEY}`,
             'Content-Type': 'application/json'
         },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: "Hello!" }],
+            max_tokens: 100
+        })
+    }
+
+    try {
+        await fetch('https://api.openai.com/v1/chat/completions', options)
+    } catch {
+
+    }
+}
+
+submitButton.addEventListener('click', getMessage)
