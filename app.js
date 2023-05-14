be1a3eb2b23697978ac9dbeaf342a0af33c21105
Dmitry Velichko <soleil-2005@mail.ru>
2023-05-14 19:18:00 +0800

    New Commit

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 
 const submitButton = document.querySelector('#submit')
+const outputElement = document.querySelector('#output')
+const inputElement = document.querySelector('input')
+const historyElement = document.querySelector('.history')
+const buttonElement = document.querySelector('button')
+
+
+function changeInput(value) {
+    const inputElement = document.querySelector('input')
+    inputElement.value = value
+}
 
 async function getMessage() {
 
@@ -11,15 +21,22 @@ async function getMessage() {
         },
         body: JSON.stringify({
             model: "gpt-3.5-turbo",
-            messages: [{ role: "user", content: "Hello!" }],
-            max_tokens: 100
+            messages: [{ role: "user", content: inputElement.value }],
+            max_tokens: 10
         })
     }
 
     try {
         const response = await fetch('https://api.openai.com/v1/chat/completions', options)
         const data = await response.json()
-        console.log (data)
+        outputElement.textContent = data.choices[0].message.content
+        if(data.choices[0].message.content) {
+            const pElement = document.createElement('p')
+            pElement.textContent = inputElement.value
+            pElement.addEventListener('click', () => changeInput(pElement.textContent))
+            historyElement.append(pElement)
+
+        }
 
     } catch (error) {
         console.error(error)
@@ -27,3 +44,9 @@ async function getMessage() {
 }
 
 submitButton.addEventListener('click', getMessage)
+
+function clearInput() {
+    inputElement.value = ''
+}
+
+buttonElement.addEventListener('click', clearInput)
