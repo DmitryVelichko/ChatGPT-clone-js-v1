8f8d18cbfc05d26e2541ba5c287a6508d3fc8f3d
Dmitry Velichko <soleil-2005@mail.ru>
2023-05-19 00:12:39 +0800

    New commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+
+
+const App = () => {
+  return (
+    <div className="app">
+      <section className="side-bar">
+        <button>+ New chat</button>
+        <ul className="history">
+          <li>sdfsdf</li>
+        </ul>
+        <nav>
+          <p>Designed by OpenAI</p>
+        </nav>
+      </section>
+      <section className="main">
+        <h1>ChatGPT</h1>
+        <ul className="feed">
+
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input />
+            <div id="submit">➢</div>
+          </div>
+          <p className="info">
+            ChatGPT Mar 14 version. Free Research Preview.
+            Our goal is to make AI systems more natural and safe to interact with. Your feedback will help us improve.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
