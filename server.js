0fbaca375e78d52bea733183b7a0128f5710c253
Dmitry Velichko <soleil-2005@mail.ru>
2023-05-19 18:35:29 +0800

    New Commit

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
+const fetch = require("node-fetch");
+
 const PORT = 8000
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-const API_KEY = ''
+const API_KEY = process.env.API_KEY;
 
 app.post('/completions', async (req, res) => {
 
-    const options ={
+    const options = {
         method: 'POST',
         headers: {
             "Authorization": `Bearer ${API_KEY}`,
@@ -17,16 +20,16 @@ app.post('/completions', async (req, res) => {
         },
         body: JSON.stringify({
             model: "gpt-3.5-turbo",
-            messages: [{role: "user", content: "works????"}],
-            max_tokens: 100,
+            messages: [{ role: "user", content: req.body.message }],
+            max_tokens: 10,
         })
     }
 
     try {
-       const response = await fetch('https://api.openai.com/v1/chat/completions', options)
-       const data = await response.json()
-       res.send(data)
-    } catch(error) {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+        const data = await response.json()
+        res.send(data)
+    } catch (error) {
         console.error(error)
     }
 })
--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-
+import { useState, useEffect } from "react"
 
 const App = () => {
+  const [value, setValue] = useState(null)
+  const [message, setMessage] = useState(null)
+  const [previousChats, setPreviousChats] = useState([])
+  const [currentTitle, setCurrentTitle] = useState(null)
+
+  const createNewChat = () => {
+    setMessage(null)
+    setValue('')
+    setCurrentTitle(null)
+  }
+
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle)
+    setMessage(null)
+    setValue('')
+  }
 
   const getMessages = async () => {
     const options = {
       method: 'POST',
       body: JSON.stringify({
-        message: "Hellow how are you"
+        message: value
       }),
       headers: {
         "Content-Type": "application/json"
@@ -16,32 +32,61 @@ const App = () => {
     try {
       const response = await fetch('http://localhost:8000/completions', options)
       const data = await response.json()
-      console.log(data)
+      setMessage(data.choices[0].message)
+
     } catch (error) {
       console.error(error)
     }
+
   }
 
+  useEffect(() => {
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value)
+    }
+    if (currentTitle && value && message) {
+      setPreviousChats(prevChats => (
+        [...prevChats,
+        {
+          title: currentTitle,
+          role: 'user',
+          content: value
+        },
+        {
+          title: currentTitle,
+          role: message.role,
+          content: message.content
+        }
+        ]
+      ))
+    }
+  }, [message, currentTitle])
+
+  const currentChat = previousChats.filter(previousChat => previousChat.title === currentTitle)
+  const uniqueTitles = Array.from(new Set(previousChats.map(previousChat => previousChat.title)))
 
   return (
     <div className="app">
       <section className="side-bar">
-        <button>+ New chat</button>
+        <button onClick={createNewChat}>+ New chat</button>
         <ul className="history">
-          <li>sdfsdf</li>
+          {uniqueTitles?.map((uniqueTitle, index) => <li onClick={() => handleClick(uniqueTitle)} key={index}>{uniqueTitle}</li>)}
         </ul>
         <nav>
           <p>Designed by OpenAI</p>
         </nav>
       </section>
       <section className="main">
-        <h1>ChatGPT</h1>
+        {!currentTitle && <h1>ChatGPT</h1>}
         <ul className="feed">
-
+          {currentChat?.map((chatMessage, index) => <li key={index}>
+            <p className="role">{chatMessage.role}</p>
+            <p>{chatMessage.content}</p>
+          </li>)}
         </ul>
         <div className="bottom-section">
           <div className="input-container">
-            <input />
+            <input value={value} onChange={(e) => setValue(e.target.value)} />
             <div id="submit" onClick={getMessages}>➢</div>
           </div>
           <p className="info">
